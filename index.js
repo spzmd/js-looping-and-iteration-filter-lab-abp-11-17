dfb77c5957b2e72458147f9fc9fc6657ec9254de
spzmd <b.keithdavid@gmail.com>
2018-04-02 23:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
 function findMatching(drivers, string) {
   return drivers.filter((e)) => { return e.toLowerCase() === string.toLowerCase(); });
 }
+  
+
+  
+/*
+function findMatching(collection, driver) {
+  for (const driver of collection) {
+    if (driver.name === 'Bobby' || 'bobby') {
+      console.log(driver.name);
+    }
+  }
+}
+
+drivers('Bobby');
+*/
