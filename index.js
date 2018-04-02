a77a76ef021381d6338a0ba922869ac5dbe51780
spzmd <b.keithdavid@gmail.com>
2018-04-02 23:03:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  expect(matchName(drivers, 'Bobby')).to.eql([
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh'
+    },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay'
+    }
+  ]);
+
+function findMatching(collection, driver) {
+  for (const driver of collection) {
+    if (driver.name === 'Bobby' || 'bobby') {
+      console.log(driver.name);
+    }
+  }
+}
+
+drivers('Bobby');
